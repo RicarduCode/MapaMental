939a19a4b89ec12dcf272df2fece71e7243c45e2
RicarduCode <ricardosouzaseramim@gmail.com>
2023-05-05 10:06:06 -0300

    talentos

---
+++ pessoa.java
@@ -0,0 +1,84 @@
+public class Pessoa {
+
+private String nome;
+private Int telefone;
+private Int cep;
+private String bairro;
+private String complemento;
+private String cidade;
+private String estado;
+private String pais;
+
+public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+
+public void setTelefone(Int telefone) {
+    this.telefone = telefone;
+  }
+
+  public Int getTelefone() {
+    return this.telefone;
+  }
+
+
+public void setCep(Int cep) {
+    this.cep = cep;
+  }
+
+  public Int getCep() {
+    return this.cep;
+  }
+
+
+public void setBairro(String bairro) {
+    this.bairro = bairro;
+  }
+
+  public String getBairro() {
+    return this.bairro;
+  }
+
+
+public void setComplemento(String complemento) {
+    this.complemento = complemento;
+  }
+
+  public String getComplemento() {
+    return this.complemento;
+  }
+
+
+public void setCidade(String cidade) {
+    this.cidade = cidade;
+  }
+
+  public String getCidade() {
+    return this.cidade;
+  }
+
+
+public void setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  public String getEstado() {
+    return this.estado;
+  }
+
+
+public void setPais(String pais) {
+    this.pais = pais;
+  }
+
+  public String getPais() {
+    return this.pais;
+  }
+
+}
+
---
+++ pessoa.js
@@ -0,0 +1,12 @@
+function Pessoa () {
+
+this.nome = String;
+this.telefone = Int;
+this.cep = Int;
+this.bairro = String;
+this.complemento = String;
+this.cidade = String;
+this.estado = String;
+this.pais = String;
+
+}
